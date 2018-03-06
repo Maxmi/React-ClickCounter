46b1eb61e5335c7432fe8d3889649f30b9ce7cd1
Gulmira Berdalieva <gulmira2410@gmail.com>
2018-03-05 17:35:46 -0800

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import PropTypes from 'prop-types';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.handlePlus = this.handlePlus.bind(this);
+        this.handleMinus = this.handleMinus.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: props.count
+        };
+    }
 
-    constructor() {
-        super();
+    handlePlus() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            };
+        });
+    }
+
+    handleMinus() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count - 1
+            };
+        });
+    }
+
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        });
     }
 
     render() {
         return (
-            <div className='container'>
-                <div className='title'>React Starter</div>
-                <img className='logo' src={logo} />
+            <div className="container">
+                <div className="counter">{this.state.count}</div>
+                <button className="plus" onClick={this.handlePlus}>
+                    +
+                </button>
+                <button className="reset" onClick={this.handleReset}>
+                    Reset
+                </button>
+                <button className="minus" onClick={this.handleMinus}>
+                    -
+                </button>
             </div>
         );
     }
 }
+
+App.defaultProps = {
+    count: 0
+};
+
+App.propTypes = {
+    count: PropTypes.number
+};
